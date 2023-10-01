25d75b3c8e2e2cc118918539b9c87fb57257846a
zangsu <zangsu_@naver.com>
2023-10-01 19:57:24 +0900

    feat : start project

---
+++ src/main/java/elecs/elecswiki/ElecswikiApplication.java
@@ -0,0 +1,13 @@
+package elecs.elecswiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElecswikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElecswikiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/elecs/elecswiki/ElecswikiApplicationTests.java
@@ -0,0 +1,13 @@
+package elecs.elecswiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElecswikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
